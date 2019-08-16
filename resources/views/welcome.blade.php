c080668ff07b3f69af8da55885198cce1c613b69
bardi <rbardi75@gmail.com>
2019-08-16 16:31:15 +0200

    Branch home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    PROJECTS BLOG UTILITY
                 </div>
 
                 <div class="links">
