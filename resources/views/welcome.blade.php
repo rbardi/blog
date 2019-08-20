1241a23cdfb2a2b801cb5d5d344345cb9e3c18ce
webdesignrb <rbardiwebdesign@gmail.com>
2019-08-20 16:04:00 +0200

    home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 5
+                    Laravel 50
                 </div>
 
                 <div class="links">
