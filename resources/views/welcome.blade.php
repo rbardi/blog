ef2dc320cb304e807e0d034f4fcd398ede90d209
webdesignrb <rbardiwebdesign@gmail.com>
2020-05-25 11:56:37 +0200

    prova commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 555
+                    Laravel prova push con sublime text 
                 </div>
 
                 <div class="links">
